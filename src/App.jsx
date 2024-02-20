2ad6ab27c9ad40cebe2c782ca67b220487278109
Kacper <kacper.smolinski@hotmail.com>
2024-02-20 15:10:52 +0100

    Countries data fetch added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+    const[data, setData] = useState([]);
+
+    useEffect(() => {
+        const url = 'https://restcountries.com/v3.1/all';
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.log(error));
+    }, []);
+
+    console.log(data);
+
   return (
-    <div>App</div>
+    <div>
+        Countries:
+        <ul>
+            {data.map((country, i) => (
+                <li key={i}>{country.name.common}: <p style={{fontSize: '4rem', margin: '0'}}>{country.flag}</p></li>
+            ))}
+        </ul>
+    </div>
   )
 }
 
