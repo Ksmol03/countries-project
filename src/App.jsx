18c91db86dcbaaca5695296cf2d5bfc8c17f894e
Kacper <kacper.smolinski@hotmail.com>
2024-02-21 13:37:33 +0100

    feat: Add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,54 @@ import React, { useEffect, useState } from 'react'
 
 const App = () => {
     const[data, setData] = useState([]);
+    const[search, setSearch] = useState('');
+    const[sortType, setSortType] = useState('alphabetic');
 
     useEffect(() => {
         const url = 'https://restcountries.com/v3.1/all';
         fetch(url)
             .then(response => response.json())
             .then(data => setData(data))
+            .then(() => {console.log("Fetched succesfully!")})
             .catch(error => console.log(error));
     }, []);
-    console.log(data);
+    
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    }
 
   return (
     <div className='container'>
         <h1>Countries</h1>
+        <div className="search">
+            <input type="search" name="search" id="search" value={search} onChange={handleSearch}/>
+            <div>
+                <p>Sort By:</p>
+            </div>
+        </div>
         <div className='flag-container'>
-        {data.map(country => {
+        {data
+        .filter(country => {
+            const nameMatch = country.name.common.toUpperCase().includes(search.toUpperCase());
+            const regionMatch = country.region.toUpperCase().includes(search.toUpperCase());
+            const capitalMatch = () => {
+                if (typeof country.capital == 'object') {
+                    return country.capital[0].toUpperCase().includes(search.toUpperCase());
+                }
+            }
+            const languageMatch = () => {
+                if (typeof country.languages == 'object') {
+                    return (
+                        Object.getOwnPropertyNames(country.languages).filter(lang => country.languages[lang].toUpperCase().includes(search.toUpperCase())).length != 0
+                    )
+                }
+            }
+
+            return (
+                nameMatch || regionMatch || capitalMatch() || languageMatch()
+            )
+        })
+        .map(country => {
             const languages =  typeof country.languages == 'object' ?  Object.getOwnPropertyNames(country.languages).map(prop => country.languages[prop]).slice(0, 3).join(', '): '';
 
             return (
