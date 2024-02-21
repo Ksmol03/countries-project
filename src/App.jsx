a17daa4d93529af8fe42dac70b2055f6f5026b5c
Kacper <kacper.smolinski@hotmail.com>
2024-02-21 14:00:53 +0100

    feat: Add sort system

--- src/App.jsx
+++ src/App.jsx
@@ -25,29 +25,50 @@ const App = () => {
             <input type="search" name="search" id="search" value={search} onChange={handleSearch}/>
             <div>
                 <p>Sort By:</p>
+                <p className="alphabetic" onClick={() => sortType == 'alphabetic' ? setSortType('alphabeticReverse') :setSortType('alphabetic') }>Alphabetic</p>
+                <p className="population" onClick={() => sortType == 'population' ? setSortType('populationReverse') :setSortType('population') }>Population</p>
             </div>
         </div>
-        <div className='flag-container'>
+        <div className='countries-container'>
         {data
         .filter(country => {
             const nameMatch = country.name.common.toUpperCase().includes(search.toUpperCase());
             const regionMatch = country.region.toUpperCase().includes(search.toUpperCase());
-            const capitalMatch = () => {
-                if (typeof country.capital == 'object') {
-                    return country.capital[0].toUpperCase().includes(search.toUpperCase());
-                }
-            }
-            const languageMatch = () => {
-                if (typeof country.languages == 'object') {
-                    return (
-                        Object.getOwnPropertyNames(country.languages).filter(lang => country.languages[lang].toUpperCase().includes(search.toUpperCase())).length != 0
-                    )
-                }
-            }
+
+            const capitalMatch = () => (typeof country.capital == 'object') && (country.capital[0].toUpperCase().includes(search.toUpperCase()));
+
+            const languageMatch = () => (typeof country.languages == 'object') && (Object.getOwnPropertyNames(country.languages).filter(lang => country.languages[lang].toUpperCase().includes(search.toUpperCase())).length != 0);
 
             return (
                 nameMatch || regionMatch || capitalMatch() || languageMatch()
             )
+        })
+        .sort((a, b) => {
+            if (sortType == 'alphabetic') {
+                if (a.name.common < b.name.common) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            } else if (sortType == 'alphabeticReverse') {
+                if (a.name.common < b.name.common) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            } else if (sortType == 'population') {
+                if (a.population < b.population) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            } else if (sortType == 'populationReverse') {
+                if (a.population < b.population) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            } 
         })
         .map(country => {
             const languages =  typeof country.languages == 'object' ?  Object.getOwnPropertyNames(country.languages).map(prop => country.languages[prop]).slice(0, 3).join(', '): '';
